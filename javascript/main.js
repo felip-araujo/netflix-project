fad6e95e996baac1e1871b1bf9168773024b3526
felip-araujo <felipearaujoexecutive@gmail.com>
2023-10-03 16:43:03 -0400

    adição de arquivos JS e algumas funcionalidades basicas

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+
+function filmeIndisponivel(){
+    alert('Esse filme está deixando o nosso catálago')
+} 
+
+function filmeParaMaiores(){ 
+    alert('Esse filme está disponível para maiores de idade') 
+    alert(button) 
+} 
+
+function criarPopup(){
+    newpopupWindow = window.open ('', 'pagina',"width=250 heigth=250"); 
+    newpopupWindow.document.write ("Esse é um POPUP e aqui você pode por o que quiser"); 
+
+} 
